e7079fbadaa3bc6e17e1f05bdc804654d93f1195
BK <brian_j_king@me.com>
2020-10-05 10:53:26 -0500

    Add project files.

---
+++ KKBC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KKBC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
