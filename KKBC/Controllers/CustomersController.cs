bdc6c929877358f861915f09f8e73c66b98a396f
BK <brian_j_king@me.com>
2020-10-05 18:09:46 -0500

    Reads data from the customers table

---
+++ KKBC/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KKBC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KKBC.Controllers
+{
+    public class CustomersController : Controller
+    {
+    
+        private readonly ICustomersRepository repo;
+
+        public CustomersController(ICustomersRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = repo.GetAllCustomers();
+            return View(customers);
+        }
+    }
+}
--- KKBC/Startup.cs
+++ KKBC/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using KKBC.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
 
 namespace KKBC
 {
@@ -23,6 +26,16 @@ namespace KKBC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDbConnection>((s) =>
+            {
+                IDbConnection conn = new MySqlConnection(Configuration.GetConnectionString("kkbc"));
+                conn.Open();
+                return conn;
+            });
+
+            services.AddTransient<ICustomersRepository, CustomersRepository>();
+
+
             services.AddControllersWithViews();
         }
 
