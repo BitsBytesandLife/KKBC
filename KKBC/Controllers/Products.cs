0d6843f7b9a86f0d2221de93f5fe5c0371a03092
BK <brian_j_king@me.com>
2020-10-08 07:09:33 -0500

    Add three models Products,Scents,Category

---
+++ KKBC/Controllers/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KKBC.Controllers
+{
+    public class Products
+    {
+        public int ProdID { get; set; }
+        public string Name { get; set; }
+        public int CatID { get; set; }
+        public int StoclLevel { get; set; }
+        public double Price { get; set; }
+    }
+}
